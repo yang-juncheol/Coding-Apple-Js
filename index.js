01c797e875f1e361923a65655070a1299af6c82b
Juncheol <Juncheol99481201@gmail.com>
2023-11-24 13:18:27 +0900

    form-test

--- index.js
+++ index.js
@@ -1,45 +1,10 @@
-$(".tab-button").on("click", function () {
-  $(".tab-button").removeClass("orange");
-});
-
-$(".tab-button")
-  .eq(0)
-  .on("click", function () {
-    $(".tab-button").eq(0).addClass("orange");
-  });
-
-$(".tab-button")
-  .eq(1)
-  .on("click", function () {
-    $(".tab-button").eq(1).addClass("orange");
-  });
-
-$(".tab-button")
-  .eq(2)
-  .on("click", function () {
-    $(".tab-button").eq(2).addClass("orange");
-  });
-
-// tab-content
-
-$(".tab-button").on("click", function () {
-  $(".tab-content").removeClass("show");
-});
-
-$(".tab-button")
-  .eq(0)
-  .on("click", function () {
-    $(".tab-content").eq(0).addClass("show");
-  });
-
-$(".tab-button")
-  .eq(1)
-  .on("click", function () {
-    $(".tab-content").eq(1).addClass("show");
-  });
-
-$(".tab-button")
-  .eq(2)
-  .on("click", function () {
-    $(".tab-content").eq(2).addClass("show");
-  });
+for (let i = 0; i < $(".tab-button").length; i++) {
+  $(".tab-button")
+    .eq(i)
+    .on("click", function () {
+      $(".tab-button").removeClass("orange");
+      $(".tab-button").eq(i).addClass("orange");
+      $(".tab-content").removeClass("show");
+      $(".tab-content").eq(i).addClass("show");
+    });
+}
