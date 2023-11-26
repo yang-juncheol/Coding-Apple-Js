968163db70c54ac2f397d2781f7e5ff0c3858d3e
juncheol <juncheol99481201@gmail.com>
2023-11-27 00:09:35 +0900

    tapOpen-test

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-for (let i = 0; i < $(".tab-button").length; i++) {
-  $(".tab-button")
-    .eq(i)
-    .on("click", function () {
-      $(".tab-button").removeClass("orange");
-      $(".tab-button").eq(i).addClass("orange");
-      $(".tab-content").removeClass("show");
-      $(".tab-content").eq(i).addClass("show");
-    });
+// tapOpen-for test
+// for (let i = 0; i < $(".tab-button").length; i++) {
+//   $(".tab-button")
+//     .eq(i)
+//     .on("click", function () {
+//       tapOpen(i);
+//     });
+// }
+
+// tapOpen
+$(".list").click(function (e) {
+  tapOpen(e.target.dataset.id);
+});
+
+function tapOpen(tapOpenCount) {
+  $(".tab-button").removeClass("orange");
+  $(".tab-button").eq(tapOpenCount).addClass("orange");
+  $(".tab-content").removeClass("show");
+  $(".tab-content").eq(tapOpenCount).addClass("show");
 }
+
+var car = { name: "소나타", price: 50000 };
+
+document.querySelectorAll("#name").innerText = car["name"];
+document.querySelectorAll("#price").innerText = car["price"];
